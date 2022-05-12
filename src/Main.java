8007f7147c1cec3294fa50f124b3ee9f47d894b2
Aleksei Bochkarev <aleksei.bochkarev@brandmaker.com>
2022-05-12 22:29:30 +0700

    add lines with Idea

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("My first text");
+        System.out.println("QA GURU");
+        System.out.println("Another");
     }
 }
